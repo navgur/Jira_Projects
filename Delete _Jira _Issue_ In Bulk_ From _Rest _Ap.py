68c996debecf0746fbefb672248437338be18d84
rani <106962628+navgur@users.noreply.github.com>
2022-11-28 01:13:26 +0530

    Update and rename tutorial 12.py to Delete _Jira _Issue_ In Bulk_ From _Rest _Ap.py

--- Delete _Jira _Issue_ In Bulk_ From _Rest _Ap.py
+++ Delete _Jira _Issue_ In Bulk_ From _Rest _Ap.py
@@ -11,4 +11,4 @@ with io.open("issue_delete.csv","r",encoding="utf-8")as f1:
 data=data.split()  
 for id in data:
     url="https://uytrfgh.atlassian.net/rest/api/3/issue/"+id
-    response=requests.delete(url,headers=headers,auth=("ranisingh21@navgurukul.org","Z6Wb0ny3miznaLMLe2xK4488"))
+    response=requests.delete(url,headers=headers,auth=("ranisingh21@navgurukul.org","RgScuWcqedhuqkFeXz0i947D"))
