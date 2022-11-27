bdfde6439421eafabe054db12ab803a1f359aa43
rani <106962628+navgur@users.noreply.github.com>
2022-11-28 01:03:26 +0530

    Update and rename tutorial 9.py to Get Status From _All _Jira _Issues _From_Rest _Api.py

--- Get Status From _All _Jira _Issues _From_Rest _Api.py
+++ Get Status From _All _Jira _Issues _From_Rest _Api.py
@@ -9,12 +9,12 @@ header={
 query={
     'jql':"project=REET"
 }
-response=requests.get(url,headers=header,params=query,auth=("ranisingh21@navgurukul.org","Z6Wb0ny3miznaLMLe2xK4488"))
+response=requests.get(url,headers=header,params=query,auth=("ranisingh21@navgurukul.org","FHgTHyHRZSSklaxa6dxRAE71"))
 data=response.json()
 issues=data["issues"]
 for issue in issues:
     issue_key=issue["key"]
     url1="https://uytrfgh.atlassian.net/rest/api/3/issue/"+issue_key
-    response=requests.get(url1,headers=header,auth=("ranisingh21@navgurukul.org","Z6Wb0ny3miznaLMLe2xK4488"))
+    response=requests.get(url1,headers=header,auth=("ranisingh21@navgurukul.org","FHgTHyHRZSSklaxa6dxRAE71"))
     data=response.json()
     print(f'issue id is {issue_key} and status is {data["fields"]["status"]["name"]}')
