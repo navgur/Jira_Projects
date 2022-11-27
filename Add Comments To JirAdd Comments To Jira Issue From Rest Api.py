7469df1797e08c5f5cc30e1bd4d123f75c893af7
rani <106962628+navgur@users.noreply.github.com>
2022-11-28 00:19:08 +0530

    Update and rename tutorial 6.py to Add Comments To JirAdd Comments To Jira Issue From Rest Api.py

--- Add Comments To JirAdd Comments To Jira Issue From Rest Api.py
+++ Add Comments To JirAdd Comments To Jira Issue From Rest Api.py
@@ -23,6 +23,5 @@ data=json.dumps({"body": {
     ]
   }
 })
-
-response=requests.post(url,headers=header,data=data,auth=("ranisingh21@navgurukul.org","Z6Wb0ny3miznaLMLe2xK4488"))
+response=requests.post(url,headers=header,data=data,auth=("ranisingh21@navgurukul.org","6vAGdOueVGtZPZETZxACF2A6"))
 print(response.text)
